06121b89425cdf8650945b23ded0c5da117885ee
Jan Motene <motenejo@gmail.com>
2023-01-07 13:40:45 +0200

    Updating Contact Section and Footer

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './contacts.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import {useRef} from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) =>{
+    e.preventDefault();
+
+    emailjs.sendForm('service_g0zc08k','template_isb2p0p', form.current,'eYH7gRd14qa6aJ8Pl')
+  
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -11,26 +25,33 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>motenejo@gmail.com</h5>
-            <a href="mailto:motenejo@gmail.com">Send an Email</a>
+            <a href="mailto:motenejo@gmail.com" target='_blank'>Send an Email</a>
           </article>
 
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>Jan Motene</h5>
-            <a href="https://">Send an Email</a>
+            <a href="https://web.facebook.com/MoteneEmjay" target='_blank'>Send a Messenger Text</a>
           </article>
 
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
-            <h5>motenejo@gmail.com</h5>
-            <a href="+2793530741">Send an Email</a>
+            <h5>+27793530741</h5>
+            <a href="https://api.whatsapp.com/send?phone+27793530741" target='_blank'>Send WhatsApp Text</a>
           </article>
         </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email Address' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
 
     </section>
