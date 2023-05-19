949cb21abaf6132f87d7cdd598d78f4b14b5dd18
Jan Motene <motenejo@gmail.com>
2023-05-19 09:09:38 +0200

    Updating deployment

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer>
       <a href="#" className='footer__logo'>JO MOTENE</a>
-
+ 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
