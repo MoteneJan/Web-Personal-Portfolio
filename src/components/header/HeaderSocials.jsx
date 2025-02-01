8ae45c13ad5eda3cc870cba3380f8ed6bce7ff4b
Jan Oupana Motene <motenejo@gmail.com>
2025-02-01 05:25:20 +0200

    Saving all modified files

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>    
-        <a href="https://github.com" target="_blank"><FaGithub/></a> 
+        <a href="https://www.linkedin.com/in/jan-motene-4864771b4/" target="_blank"><BsLinkedin/></a>    
+        <a href="https://github.com/MoteneJan/" target="_blank"><FaGithub/></a> 
          
     </div>
   )
