3a3f4dbb0c08d84da6313ca92f8d21834e1dcebf
Jan Oupana Motene <motenejo@gmail.com>
2025-02-23 12:20:07 +0200

    Final Commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/MOTENE JO- RESUME.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download Resume</a>
+        <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Talk To Me!</a>
     </div>
   )
